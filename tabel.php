0c23c789e13d0d1e9b94c55cf4610aabd8e7dc33
Andriana0121 <56000335+Andriana0121@users.noreply.github.com>
2020-09-07 16:10:53 +0300

    informatia in format tabelar

---
+++ tabel.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+   <?php
+
+        echo "<table border='4' class='stats' cellspacing='0'>
+            <tr>
+            <th>ID</th>
+            <th>Denumire</th>
+            <th>Categorie</th>
+            <th>Pret</th>
+
+            </tr>";
+
+              echo "<tr>";
+              echo "<td>" . "001" . "</td>";
+              echo "<td>" . "Aaa" . "</td>";
+              echo "<td>" . "First" . "</td>";
+              echo "<td>" . "12$" . "</td>";
+
+              echo "</tr>";
+
+              echo "<tr>";
+              echo "<td>" . "002" . "</td>";
+              echo "<td>" . "BBB" . "</td>";
+              echo "<td>" . "second" . "</td>";
+              echo "<td>" . "5$" . "</td>";
+
+              echo "</tr>";
+
+              echo "<tr>";
+              echo "<td>" . "003" . "</td>";
+              echo "<td>" . "CCC" . "</td>";
+              echo "<td>" . "Third" . "</td>";
+              echo "<td>" . "2$" . "</td>";
+
+              echo "</tr>";
+
+    echo "</table>";
+
+?>
+</body>
+</html>
